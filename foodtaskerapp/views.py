1553a326fafa599389954671514900d5e2147c3a
KhanhND <khanhnd@KhanhNDs-iMac.local>
2017-09-24 00:20:45 +0700

    sign-in & sign out

---
+++ foodtaskerapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return redirect(restaurant_home)
+
+"""
+check authen before switch to restaurant page
+"""
+@login_required(login_url = '/restaurant/sign-in/')
+
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html')
