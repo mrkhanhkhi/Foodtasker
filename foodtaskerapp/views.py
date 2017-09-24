f896a08c81fa155d2f941036d547dbf63efb867c
KhanhND <khanhnd@KhanhNDs-iMac.local>
2017-09-24 22:22:47 +0700

    add sign-up screen

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from foodtaskerapp.forms import UserForm, RestaurantForm
 
 # Create your views here.
 def home(request):
@@ -11,4 +12,13 @@ check authen before switch to restaurant page
 @login_required(login_url = '/restaurant/sign-in/')
 
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html')
+    return render(request, 'restaurant/home.html', {})
+
+def restaurant_sign_up(request):
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+
+    return render(request, 'restaurant/sign_up.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
