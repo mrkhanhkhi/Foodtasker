f95332d11fa93652407865a9d6386b9df9c85dff
Nguyen Duy Khanh B <nguyen.duy.khanhb@framgia.com>
2017-12-07 22:33:16 +0700

    fix8

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -127,5 +127,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 import dj_database_url
-db_from_env = dj_databse_url.config()
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
