4bdb3279dec1f0bade1576137a3780db76e0fdba
Nguyen Duy Khanh B <nguyen.duy.khanhb@framgia.com>
2017-12-19 20:53:38 +0700

    add Bootstrap

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -12,9 +12,24 @@ def home(request):
 check authen before switch to restaurant page
 """
 @login_required(login_url = '/restaurant/sign-in/')
-
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html', {})
+    return render(request, 'restaurant/base.html', {})
+
+@login_required(login_url = '/restaurant/sign-in/')
+def restaurant_account(request):
+    return render(request, 'restaurant/account.html', {})
+
+@login_required(login_url = '/restaurant/sign-in/')
+def restaurant_meal(request):
+    return render(request, 'restaurant/meal.html', {})
+
+@login_required(login_url = '/restaurant/sign-in/')
+def restaurant_order(request):
+    return render(request, 'restaurant/order.html', {})
+
+@login_required(login_url = '/restaurant/sign-in/')
+def restaurant_report(request):
+    return render(request, 'restaurant/report.html', {})
 
 def restaurant_sign_up(request):
     user_form = UserForm()
